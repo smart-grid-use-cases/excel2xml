0de513f7eed49d239435d62e1e823aec0f9c63ac
Richard Marston <rmarston@eonerc.rwth-aachen.de>
2020-09-14 11:36:30 +0200

    Create repo trigger for xml2md

--- process_all_xlsx.sh
+++ process_all_xlsx.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
+if [ -z "$1" ]
+then
+  OUTPUT_DIR="/github/workspace/grupoetra"
+else
+  OUTPUT_DIR="$1"
+fi
+
+mkdir -p ${OUTPUT_DIR}
+
 find /excel2xml -type f -name '*.xlsx' -exec sh -c '
   file="$0"
-  python3 /excel2xml/xlsx2xml.py "$file"
-' {} ';'
+  OUTPUT_DIR="$1"
+  file_no_ext="${file%.xlsx}"
+  DIRNAME=$(basename "${file_no_ext}" | tr '[:blank:]' '_')
+  mkdir -p ${OUTPUT_DIR}/${DIRNAME}
+  output_file_name="${OUTPUT_DIR}/${DIRNAME}/${DIRNAME}.xml"
+  echo "$output_file_name"
+  python3 /excel2xml/xlsx2xml.py "$file" > "$output_file_name"
+' {} ${OUTPUT_DIR} ';'
