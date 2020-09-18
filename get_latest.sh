0de3a3e92844a9da9036a766c54c6d3450353420
Richard Marston <rmarston@eonerc.rwth-aachen.de>
2020-09-18 11:55:21 +0200

    Make sure we download the correct version of excel2xml-input
    Signed-off-by: Richard Marston <rmarston@eonerc.rwth-aachen.de>

--- get_latest.sh
+++ get_latest.sh
@@ -2,18 +2,31 @@ owner_url=$1
 repo_name=$2
 file_request=$3
 destination=$4
-latest_url=$(wget https://github.com/${owner_url}/${repo_name}/releases/latest --max-redirect=0 2>&1 | grep Location)
-prefix="Location: https://github.com/${owner_url}/${repo_name}/releases/tag/"
-suffix=" [following]"
-tail=${latest_url#"$prefix"}
-release_version=${tail%"$suffix"}
-echo "Release version: $release_version"
-if [ -z "${file_request}" ]
-then
-    exit 0
-else
+release_version=$5
+echo "Requesting release version: $release_version"
+wget_received=8
+retry_count=0
+while [ $wget_received -eq 8 ]
+do
     wget -q https://github.com/${owner_url}/${repo_name}/releases/download/${release_version}/${file_request}
+    wget_received=$?
+    echo "wget received: $wget_received"
+    if [ $wget_received -ne 0 ]
+    then
+        ((retry_count=$retry_count+1))
+        if [ $retry_count -gt 10 ];
+        then
+            break
+        else
+            sleep 5
+        fi
+    fi
+
+done
+if [ $wget_received -eq 0 ];
+then
     unzip -q $file_request -d $destination
     rm $file_request
     exit 0
 fi
+exit 1
