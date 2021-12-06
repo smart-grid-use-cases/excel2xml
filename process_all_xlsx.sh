80663be775e8f3c712e98d47f263af4cadade5a8
Richard Marston <rmarston@eonerc.rwth-aachen.de>
2021-12-06 13:03:19 +0100

    Copy image files when creating xml
    Signed-off-by: Richard Marston <rmarston@eonerc.rwth-aachen.de>

--- process_all_xlsx.sh
+++ process_all_xlsx.sh
@@ -2,14 +2,21 @@
 
 if [ -z "$1" ]
 then
+  INPUT_DIR="/excel2xml/excel2xml-input/excel-use-cases"
+  export PYTHON_SCRIPT="/excel2xml/xlsx2xml.py"
+else
+  INPUT_DIR="$1"
+  export PYTHON_SCRIPT="xlsx2xml.py"
+fi
+
+if [ -z "$2" ]
+then
   OUTPUT_DIR="/github/workspace/grupoetra"
 else
-  OUTPUT_DIR="$1"
+  OUTPUT_DIR="$2"
 fi
 
 mkdir -p ${OUTPUT_DIR}
-
-INPUT_DIR="/excel2xml/excel2xml-input/excel-use-cases"
 
 find $INPUT_DIR -type f -name '*.xlsx' -exec sh -c '
   FILE="$0"
@@ -20,7 +27,8 @@ find $INPUT_DIR -type f -name '*.xlsx' -exec sh -c '
   BASEPATH=$(dirname "$NO_PREFIX")
   DIRNAME=$(echo "${BASEPATH}" | tr '[:blank:]' '_')
   mkdir -p ${OUTPUT_DIR}/${DIRNAME}
+  sh process_images.sh ${INPUT_DIR}/${BASEPATH} $OUTPUT_DIR/$DIRNAME
   OUTPUT_FILE_NAME="${OUTPUT_DIR}${DIRNAME}/${BASENAME}.xml"
   echo "$OUTPUT_FILE_NAME"
-  python3 /excel2xml/xlsx2xml.py "$FILE" > "$OUTPUT_FILE_NAME"
+  python3 ${PYTHON_SCRIPT} "$FILE" > "$OUTPUT_FILE_NAME"
 ' {} ${OUTPUT_DIR} ${INPUT_DIR} ';'
