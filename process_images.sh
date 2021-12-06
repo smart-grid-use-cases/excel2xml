ba48515770167538a83f25aeab44536f2fbe3978
Richard Marston <rmarston@eonerc.rwth-aachen.de>
2021-12-06 14:52:47 +0100

    Remove spaces from file names when copying images
    Signed-off-by: Richard Marston <rmarston@eonerc.rwth-aachen.de>

--- process_images.sh
+++ process_images.sh
@@ -2,10 +2,9 @@
 INPUT_DIR=$1
 OUTPUT_DIR=$2
 
-set -x
-
 find $INPUT_DIR -type f -not -name '*.xlsx' -exec sh -c '
   FILE="$0"
   OUTPUT_DIR="$1"
-  cp "$FILE" "$OUTPUT_DIR/"
+  FILE_NO_SPACES=$(basename "${FILE}" | tr '[:blank:]' '_')
+  cp "$FILE" "$OUTPUT_DIR/$FILE_NO_SPACES"
 ' {} ${OUTPUT_DIR} ';'
